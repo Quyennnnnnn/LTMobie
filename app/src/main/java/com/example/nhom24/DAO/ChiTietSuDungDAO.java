b463b9578945d0ed773b3a25ed0e385acf5eea78
Vuongdao21 <vuongds2111@gmail.com>
2025-04-13 11:59:59 +0700

    DAO chi tiết sử dụng

--- app/src/main/java/com/example/nhom24/DAO/ChiTietSuDungDAO.java
+++ app/src/main/java/com/example/nhom24/DAO/ChiTietSuDungDAO.java
@@ -1,4 +1,26 @@
 package com.example.nhom24.DAO;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.nhom24.Model.ChiTietSuDung;
+
+import java.util.List;
+
+@Dao
 public interface ChiTietSuDungDAO {
+    @Insert
+    void insert(ChiTietSuDung chiTietSuDung);
+
+    @Update
+    void update(ChiTietSuDung chiTietSuDung);
+
+    @Delete
+    void delete(ChiTietSuDung chiTietSuDung);
+
+    @Query("SELECT * FROM chi_tiet_su_dung")
+    List<ChiTietSuDung> getAllChiTietSuDung();
 }
