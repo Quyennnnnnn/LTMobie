e7ef717857f45c40f03b4df4b8455dde7f1045b5
Vuongdao21 <vuongds2111@gmail.com>
2025-04-13 14:33:44 +0700

    Tao

--- app/src/main/java/com/example/nhom24/Model/PhongHoc.java
+++ app/src/main/java/com/example/nhom24/Model/PhongHoc.java
@@ -1,4 +1,41 @@
 package com.example.nhom24.Model;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "phong_hoc")
 public class PhongHoc {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String maPhongHoc;
+    private String tenPhongHoc;
+
+    public PhongHoc(String maPhongHoc, String tenPhongHoc) {
+        this.maPhongHoc = maPhongHoc;
+        this.tenPhongHoc = tenPhongHoc;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMaPhongHoc() {
+        return maPhongHoc;
+    }
+
+    public void setMaPhongHoc(String maPhongHoc) {
+        this.maPhongHoc = maPhongHoc;
+    }
+
+    public String getTenPhongHoc() {
+        return tenPhongHoc;
+    }
+
+    public void setTenPhongHoc(String tenPhongHoc) {
+        this.tenPhongHoc = tenPhongHoc;
+    }
 }
