ed65d90868b0e42daf5f2a59b8030ca29a73ed71
Vuongdao21 <vuongds2111@gmail.com>
2025-04-13 12:10:03 +0700

    Update database

--- app/src/main/java/com/example/nhom24/Database/AppDatabase.java
+++ app/src/main/java/com/example/nhom24/Database/AppDatabase.java
@@ -6,13 +6,18 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.nhom24.DAO.ChiTietSuDungDAO;
 import com.example.nhom24.DAO.LoaiThietBiDAO;
+import com.example.nhom24.DAO.ThietBiDAO;
+import com.example.nhom24.DAO.PhongHocDAO;
 import com.example.nhom24.DAO.UserDAO;
+import com.example.nhom24.Model.ChiTietSuDung;
 import com.example.nhom24.Model.LoaiThietBi;
+import com.example.nhom24.Model.PhongHoc;
 import com.example.nhom24.Model.ThietBi;
 import com.example.nhom24.Model.User;
 
-@Database(entities = {User.class, LoaiThietBi.class, ThietBi.class }, version = 2)
+@Database(entities = {User.class, LoaiThietBi.class, ThietBi.class, PhongHoc.class, ChiTietSuDung.class}, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
     private static final String DATABASE_NAME = "app4.db";
     private static AppDatabase instance;
@@ -30,5 +35,7 @@ public abstract class AppDatabase extends RoomDatabase {
 
     public abstract UserDAO userDAO();
     public abstract LoaiThietBiDAO loaiThietBiDAO();
-    public abstract com.example.nhom24.Database.ThietBiDAO thietBiDAO();
+    public abstract ThietBiDAO thietBiDAO();
+    public abstract PhongHocDAO phongHocDAO();
+    public abstract ChiTietSuDungDAO chiTietSuDungDAO();
 }
