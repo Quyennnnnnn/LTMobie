6bcb5cb78bb0f17952a606bed79f490f0496498b
Vuongdao21 <vuongds2111@gmail.com>
2025-04-13 13:56:13 +0700

    Tạo model chi tiết sử dụng

--- app/src/main/java/com/example/nhom24/Model/ChiTietSuDung.java
+++ app/src/main/java/com/example/nhom24/Model/ChiTietSuDung.java
@@ -1,4 +1,51 @@
 package com.example.nhom24.Model;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "chi_tiet_su_dung")
 public class ChiTietSuDung {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private int phongHocId;
+    private int thietBiId;
+    private String ngaySuDung;
+
+    public ChiTietSuDung(int phongHocId, int thietBiId, String ngaySuDung) {
+        this.phongHocId = phongHocId;
+        this.thietBiId = thietBiId;
+        this.ngaySuDung = ngaySuDung;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPhongHocId() {
+        return phongHocId;
+    }
+
+    public void setPhongHocId(int phongHocId) {
+        this.phongHocId = phongHocId;
+    }
+
+    public int getThietBiId() {
+        return thietBiId;
+    }
+
+    public void setThietBiId(int thietBiId) {
+        this.thietBiId = thietBiId;
+    }
+
+    public String getNgaySuDung() {
+        return ngaySuDung;
+    }
+
+    public void setNgaySuDung(String ngaySuDung) {
+        this.ngaySuDung = ngaySuDung;
+    }
 }
