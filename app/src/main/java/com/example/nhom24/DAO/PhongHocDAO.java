93fea9fed506cceffdaa866fe0a1152ad5302ab1
Vuongdao21 <vuongds2111@gmail.com>
2025-04-13 12:02:54 +0700

    DAO phòng học

--- app/src/main/java/com/example/nhom24/DAO/PhongHocDAO.java
+++ app/src/main/java/com/example/nhom24/DAO/PhongHocDAO.java
@@ -1,4 +1,27 @@
 package com.example.nhom24.DAO;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.nhom24.Model.PhongHoc;
+
+import java.util.List;
+
+@Dao
 public interface PhongHocDAO {
+    @Query("SELECT * FROM phong_hoc")
+    List<PhongHoc> getAll();
+    @Insert
+    void insert(PhongHoc phongHoc);
+
+    @Update
+    void update(PhongHoc phongHoc);
+
+    @Delete
+    void delete(PhongHoc phongHoc);
+
+
 }
